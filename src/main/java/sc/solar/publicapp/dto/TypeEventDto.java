fefc4a2002062567f95a0488ba0df759a86b5ee9
konstantin <k.b.koval@gmail.com>
2025-10-28 15:36:35 +0300

    sse

--- src/main/java/sc/solar/publicapp/dto/TypeEventDto.java
+++
@@ -1,3 +0,0 @@
-package sc.solar.publicapp.dto;
-
-public record TypeEventDto(String typeEvent){}
--- src/main/java/sc/solar/publicapp/services/SseNotifications.java
+++
@@ -1,9 +0,0 @@
-package sc.solar.publicapp.services;
-
-import org.springframework.http.codec.ServerSentEvent;
-import reactor.core.publisher.Flux;
-
-public interface SseNotifications {
-    public Flux<ServerSentEvent> addSubscriptions(String email);
-    public void sendNotifications(String email);
-}
